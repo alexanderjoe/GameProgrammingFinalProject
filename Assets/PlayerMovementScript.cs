ac996a98b4c594ea05fdb49e54878669afac73c3
Michael C Brueggman <92526255+MCBrueggman@users.noreply.github.com>
2023-11-29 10:34:39 -0500

    simple movement
    added movement to player and disabled player gravity

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+    float moveSpeed = 5.0f;
+    Rigidbody2D rb;
+    float xDirection = 0.0f;
+    float yDirection = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        yDirection = Input.GetAxisRaw("Vertical");
+        xDirection = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(xDirection * moveSpeed, yDirection * moveSpeed);
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         
     }
