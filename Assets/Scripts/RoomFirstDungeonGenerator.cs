e92397041d2133a540e055ab1b4d7446f75132a2
Michael C Brueggman <92526255+MCBrueggman@users.noreply.github.com>
2023-12-05 20:45:11 -0500

    square rooms now connected
    rectangular rooms can now be connected to each other

--- Assets/Scripts/RoomFirstDungeonGenerator.cs
+++ Assets/Scripts/RoomFirstDungeonGenerator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RoomFirstDungeonGenerator : SimpleWalkGenerator
 {
@@ -26,10 +27,88 @@ public class RoomFirstDungeonGenerator : SimpleWalkGenerator
             (dungeonHeight, dungeonHeight, 0)), minRoomWidth, minRoomHeight);
 
         HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+
         floor = CreateSimpleRooms(roomList);
 
+        List<Vector2Int> roomCenters = new List<Vector2Int>();
+        foreach(var room in roomList)
+        {
+            roomCenters.Add((Vector2Int)Vector3Int.RoundToInt(room.center));
+        }
+
+        HashSet<Vector2Int> hallways = ConnectRooms(roomCenters);
+        floor.UnionWith(hallways);
+
         tilemapVisualizer.PaintFloorTiles(floor);
+
         WallGenerator.CreateWalls(floor, tilemapVisualizer);
+    }
+
+    private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)
+    {
+        HashSet<Vector2Int> hallways = new HashSet<Vector2Int>();
+        var currentRoomCenter = roomCenters[Random.Range(0, roomCenters.Count)];
+
+        roomCenters.Remove(currentRoomCenter);
+        while(roomCenters.Count > 0) 
+        {
+            Vector2Int closest = FindClosestPointTo(currentRoomCenter, roomCenters);
+            roomCenters.Remove(closest);
+            HashSet<Vector2Int> newHallway = CreateHallway(currentRoomCenter, closest);
+            currentRoomCenter = closest;
+            hallways.UnionWith(newHallway);
+        }
+
+        return hallways;
+    }
+
+    private HashSet<Vector2Int> CreateHallway(Vector2Int currentRoomCenter, Vector2Int destination)
+    {
+        HashSet<Vector2Int> hallway = new HashSet<Vector2Int>();
+        var position = currentRoomCenter;
+
+        hallway.Add(position);
+        while(position.y != destination.y)
+        {
+            if(destination.y > position.y)
+            {
+                position += Vector2Int.up;
+            }
+            else if(destination.y < position.y)
+            {
+                position += Vector2Int.down;
+            }
+            hallway.Add(position);
+        }
+        while (position.x != destination.x)
+        {
+            if (destination.x > position.x)
+            {
+                position += Vector2Int.right;
+            }
+            else if (destination.x < position.x)
+            {
+                position += Vector2Int.left;
+            }
+            hallway.Add(position);
+        }
+        return hallway;
+    }
+
+    private Vector2Int FindClosestPointTo(Vector2Int currentRoomCenter, List<Vector2Int> roomCenters)
+    {
+        Vector2Int closest = Vector2Int.zero;
+        float dist = float.MaxValue;
+        foreach(var position in roomCenters)
+        {
+            float currDist = Vector2.Distance(position, currentRoomCenter);
+            if(currDist < dist)
+            {
+                dist = currDist;
+                closest = position;
+            }
+        }
+        return closest;
     }
 
     private HashSet<Vector2Int> CreateSimpleRooms(List<BoundsInt> roomList)
