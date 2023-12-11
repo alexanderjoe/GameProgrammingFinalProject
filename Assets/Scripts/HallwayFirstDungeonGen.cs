bd0d86096dc74ed5448afdf470eb2bffec44eb8b
Michael C Brueggman <92526255+MCBrueggman@users.noreply.github.com>
2023-12-11 13:11:53 -0500

    created some globals
    made some globals to better store and manage the map

--- Assets/Scripts/HallwayFirstDungeonGen.cs
+++ Assets/Scripts/HallwayFirstDungeonGen.cs
@@ -13,6 +13,14 @@ public class HallwayFirstDungeonGen : SimpleWalkGenerator
     [Range(0.1f, 1)]
     private float roomPercent = 0.8f;
 
+    //PCG Data
+    private Dictionary<Vector2Int, HashSet<Vector2Int>> roomsDictionary = new Dictionary<Vector2Int, HashSet<Vector2Int>>();
+    private HashSet<Vector2Int> floorPositions, hallPositions;
+
+    //Gizmos Data
+    private List<Color> roomColors = new List<Color>();
+    [SerializeField]
+    private bool showRoomGizmo = false, showFloorGizmo;
 
     protected override void RunProceduralGeneration()
     {
